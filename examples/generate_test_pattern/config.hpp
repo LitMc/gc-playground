c8eb13d687d4b2d1dce0c66b7d5026b4dfa5c27c
jln <edfjln@gmail.com>
2026-02-01 20:49:35 +0900

    Joybusへの変換で共通となるパラメータをconfigに定義

---
+++ examples/generate_test_pattern/config.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "joybus_protocol.hpp"
+
+namespace ConvertGcInput::config {
+// Pico -> Padでのポーリングに使用するモードはMode3に固定
+// Mode3だと、未使用のAとBのアナログ入力が犠牲になり都合がいい
+constexpr Joybus::PollMode kPadQueryPollMode = Joybus::PollMode::Mode3;
+} // namespace ConvertGcInput::config
