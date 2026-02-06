a63156fcad06fdaf38ab4792a8a250dc9ce2f8d6
jln <edfjln@gmail.com>
2026-02-07 01:26:14 +0900

    動画の幅、高さ、fps、長さ、FOURCCコードを表示

--- tools/play_video.py
+++ tools/play_video.py
@@ -7,6 +7,26 @@ ap.add_argument("-v", "--video", required=True, help="path to input video file")
 
 cap = cv.VideoCapture(ap.parse_args().video)
 
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+print(f"Video resolution: {width} x {height}")
+
+fps = cap.get(cv.CAP_PROP_FPS)
+print(f"Frames per second: {fps}")
+
+frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+print(f"Total number of frames: {frame_count}")
+
+total_seconds = frame_count / fps
+hour = int(total_seconds / 3600)
+min = int((total_seconds - hour * 3600) / 60)
+sec = int(total_seconds - hour * 3600 - min * 60)
+frac = total_seconds - int(total_seconds)
+print(f"Total duration: {hour:02}:{min:02}:{sec:02}.{int(frac * 100):02}")
+
+fourcc = int(cap.get(cv.CAP_PROP_FOURCC))
+print(f"FOURCC code: {fourcc.to_bytes(4, byteorder='little')}")
+
 while cap.isOpened():
     ret, frame = cap.read()
 
